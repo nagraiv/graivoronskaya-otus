8d44de6fbfa307d6c40e9183444936da4d724e76
GraN <117268180+nagraiv@users.noreply.github.com>
2023-11-26 22:42:54 +0700

    task 6 is done

--- 6_streams/index.js
+++ 6_streams/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
-const { Transform } = require("stream");
 
+// объект промежуточного представления текста в виде словаря
 const wordsIndex = {};
 
 function splitLineIntoWords(line) {
@@ -11,9 +11,8 @@ function splitLineIntoWords(line) {
         .split(' ')
         .filter(el => !!el);
     words.forEach(word => word in wordsIndex ? wordsIndex[word] += 1 : wordsIndex[word] = 1);
-    console.log(words);
-    console.log(wordsIndex);
-    // return JSON.stringify(wordsIndex);
+    // console.log(words);
+    // console.log(wordsIndex);
 }
 
 function sortWords(wordsObj) {
@@ -21,61 +20,28 @@ function sortWords(wordsObj) {
     Object.keys(wordsObj)
         .sort()
         .map(key => vector.push(wordsObj[key]));
-    console.log(vector);
+    // console.log(vector);
     return vector.toString();
 }
 
-// splitLineIntoWords('+1 балл – использован     неоптимальный способ достижения результата\n' +
-//     '+2 балл – оптимальный способ достижения результата использован дз:частично');
-// sortWords(wordsIndex);
-// splitLineIntoWords('Критерии оценки: 1.Факт свершения действия - ДЗ отправлена на проверку');
-// sortWords(wordsIndex);
-
 (async () => {
-    const readStream = fs.createReadStream(__dirname + '/data/file4.txt', { encoding: 'utf8' });
+    const readStream = fs.createReadStream(__dirname + '/data/file5.txt', { encoding: 'utf8' });
     const writeStream = fs.createWriteStream(__dirname + '/data/output.txt', { encoding: 'utf8' });
 
-    const transformStream = new Transform({
-        transform(chunk, encoding, callback) {
-            try {
-                console.log(chunk);
-                callback(null, chunk.toString().toLowerCase());
-            } catch (e) {
-                console.log('Error occurred: ', e);
-                callback(e);
-            }
-        },
-    });
-
-// читаем по строке, чтобы chunk не разрезал слово
+    // читаем по строке, чтобы chunk не разрезал слово
     const rl = readline.createInterface({
         input: readStream,
         crlfDelay: Infinity
     });
 
-    // for await (const chunk of rl) {
-    //     // transformStream.write(chunk + '\n');
-    //     console.log(chunk);
-    //     writeStream.write(chunk + '\n');
-    // }
-    //
-    // writeStream.write('end of file');
-
+    // каждую строку разбиваем на слова и индексируем их в объекте wordsIndex
     rl.on('line', chunk => {
         splitLineIntoWords(chunk);
-        // console.log(chunk);
-        // writeStream.write(chunk + '\n');
-        // transformStream.write(chunk + '\n');
-    })
+     })
 
+    // когда весь текст прочитан, сортируем слова (ключи объекта wordsIndex) и выводим вектор количества слов
     rl.on('close',function() {
         writeStream.write( sortWords(wordsIndex) );
-        // writeStream.write('end of file');
-        // transformStream.write('end of file');
     });
-
-    // readStream.close();
-    // writeStream.close();
-    // transformStream.pipe(writeStream);
 
 })();
